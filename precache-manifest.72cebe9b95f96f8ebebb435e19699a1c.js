0ecc6f2f68a8b6310a7684a9208d338680be2dea
Osvalds Neiders <osvalds.neiders@gmail.com>
2019-01-03 22:56:27 +0200

    Updates

---
+++ precache-manifest.72cebe9b95f96f8ebebb435e19699a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9fb3958793b451cdd2",
+    "url": "/credit-landing/static/js/runtime~main.2f9fb395.js"
+  },
+  {
+    "revision": "7108ed8d8932ea6d6e2a",
+    "url": "/credit-landing/static/js/main.7108ed8d.chunk.js"
+  },
+  {
+    "revision": "6e058977329fcf8f24db",
+    "url": "/credit-landing/static/js/1.6e058977.chunk.js"
+  },
+  {
+    "revision": "7108ed8d8932ea6d6e2a",
+    "url": "/credit-landing/static/css/main.dcd13b07.chunk.css"
+  },
+  {
+    "revision": "766d8d5a4a3570f69711492f6398b4a6",
+    "url": "/credit-landing/index.html"
+  }
+];
